5788bb45854298f2bba081f1988b23cf26602699
Varshitha596 <koilakondavarshapraveenkumar@gmail.com>
2026-03-28 10:11:29 +0530

    react

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import UserDetails from './component/UserProfile/index.jsx'
+
+const givenDetails=[
+{ uniqueNo:1,
+    name:'varshitha',
+    work:'software_engineering',
+    image:'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    },
+    {
+        uniqueNo:2,
+    name:'varshitha',
+    work:'software_engineering',
+    image:'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    },
+    {
+        uniqueNo:3,
+    name:'varshitha',
+    work:'software_engineering',
+    image:'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    }
+    
+]
+const App = () => 
+(
+    <div>
+        <h1>list name</h1>
+        <ul>
+            {
+            givenDetails.map((eachItem) => (
+                 <UserDetails givenData={eachItem} key={eachItem.uniqueNo}/>
+            ))}
+        </ul>
+    </div>
+)
+export default App
