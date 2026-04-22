85a0ff8fe3e0e8e9c247c64f8d0ee627d3f83c00
Varshitha596 <koilakondavarshapraveenkumar@gmail.com>
2026-04-22 18:41:34 +0530

    nxt-demand-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,114 @@
-import {useState} from "react"
 
-const LikeButton = () => {
-const [nooflike,setnooflikes]=useState(0);
-const [whetherLiked,setwhetherLiked]=useState(false);
-const whetherLikedit_or_not = () =>{
-    setwhetherLiked(!whetherLiked);
-    setnooflikes(whetherLiked ? nooflike-1 : nooflike+1);
-};
+import TabItem from './components/TabItem/index.jsx'
+import ProjectItem from './components/ProjectItem/index.jsx'
+import Header from './components/Header/index.jsx'
 
-return (
-<div>
-    <p>NO_of_likes ❤️:{nooflike}</p>
-    <button onClick={whetherLikedit_or_not}>
-        {whetherLiked ? "Notliked":"Liked"}
-        </button>
-</div>
-);
-};
-export default LikeButton;
+import './App.css'
+
+const tabsList = [
+  {tabId: 'STATIC', displayText: 'Static'},
+  {tabId: 'RESPONSIVE', displayText: 'Responsive'},
+  {tabId: 'DYNAMIC', displayText: 'Dynamic'},
+]
+
+const projectsList = [
+  {
+    projectId: 0,
+    category: 'STATIC',
+    imageURL: 'https://assets.ccbp.in/frontend/react-js/projects-s3-img.png',
+    title: 'Music Page',
+    description:
+      'The music page enables the users to browse through the images of all-time favorite music albums.',
+  },
+  {
+    projectId: 1,
+    category: 'STATIC',
+    imageURL: 'https://assets.ccbp.in/frontend/react-js/projects-s4-img.png',
+    title: 'Tourism Website',
+    description:
+      'A tourism website enables the user to browse through the images of popular destinations.',
+  },
+  {
+    projectId: 2,
+    category: 'STATIC',
+    imageURL: 'https://assets.ccbp.in/frontend/react-js/projects-s1-img.png',
+    title: 'Advanced Technologies',
+    description:
+      'A website that gives you a basic understanding of Advanced Technologies.',
+  },
+  {
+    projectId: 4,
+    category: 'RESPONSIVE',
+    imageURL: 'https://assets.ccbp.in/frontend/react-js/projects-r4-img.png',
+    title: 'VR Website',
+    description:
+      'VR Website enables users to explore AR and VR Products and Industry happenings.',
+  },
+  {
+    projectId: 5,
+    category: 'RESPONSIVE',
+    imageURL: 'https://assets.ccbp.in/frontend/react-js/projects-r2-img.png',
+    title: 'Food Munch',
+    description: 'Food Much Website is a user-centric food tech website.',
+  },
+  {
+    projectId: 6,
+    category: 'RESPONSIVE',
+    imageURL: 'https://assets.ccbp.in/frontend/react-js/projects-r3-img.png',
+    title: 'Portfolio',
+    description:
+      'A portfolio is the best alternative for a resume to showcase your skills to the digital world.',
+  },
+  {
+    projectId: 8,
+    category: 'DYNAMIC',
+    imageURL: 'https://assets.ccbp.in/frontend/react-js/projects-d3-img.png',
+    title: 'Speed Typing Test',
+    description:
+      'Speed Typing Test Application is capable of calculating the time to type the randomly generated quote.',
+  },
+  {
+    projectId: 9,
+    category: 'DYNAMIC',
+    imageURL: 'https://assets.ccbp.in/frontend/react-js/projects-d1-img.png',
+    title: 'Random Joke Page',
+    description:
+      'Random Joke Page is an API-based dynamic Web Application that generates a new joke.',
+  },
+  {
+    projectId: 10,
+    category: 'DYNAMIC',
+    imageURL: 'https://assets.ccbp.in/frontend/react-js/projects-d2-img.png',
+    title: 'Sizing An Image',
+    description:
+      'This is a dynamic web application capable of adjusting the size of an element using DOM manipulations.',
+  },
+]
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <Header />
+      <h1 className="title">Projects</h1>
+      <p className="description">
+        Your skills and achievements showcase your strengths and abilities. Speak about any new skills or software you learnt to perform the project responsibilities.
+      </p>
+
+      <ul className="tabs-container">
+        {tabsList.map(tabDetails => (
+          <TabItem key={tabDetails.tabId} tabDetails={tabDetails} />
+        ))}
+      </ul>
+
+      <ul className="project-list-container">
+        {projectsList.map(projectDetails => (
+          <ProjectItem
+            key={projectDetails.projectId} projectDetails={projectDetails}
+          />
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Index from './App.jsx'
+import App from './App.jsx'
 // import Page from './component/UserProfile/index.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Index />
+    <App />
   </StrictMode>,
 )
