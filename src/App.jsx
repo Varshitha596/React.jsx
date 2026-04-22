4c36122fcbe88ee45c161177979a0db11cec765f
Varshitha596 <koilakondavarshapraveenkumar@gmail.com>
2026-04-22 11:47:25 +0530

    like or unlike

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import Page from "./component/UserProfile/index"
+import {useState} from "react"
 
-const userprofile={
-  name:'varsha',
-  roll:'2dmde',
-  clas:'239',
-}
+const LikeButton = () => {
+const [nooflike,setnooflikes]=useState(0);
+const [whetherLiked,setwhetherLiked]=useState(false);
+const whetherLikedit_or_not = () =>{
+    setwhetherLiked(!whetherLiked);
+    setnooflikes(whetherLiked ? nooflike-1 : nooflike+1);
+};
 
-const Index = ()=> <Page help={userprofile}/>
-
-export default Index;
+return (
+<div>
+    <p>NO_of_likes ❤️:{nooflike}</p>
+    <button onClick={whetherLikedit_or_not}>
+        {whetherLiked ? "Notliked":"Liked"}
+        </button>
+</div>
+);
+};
+export default LikeButton;
