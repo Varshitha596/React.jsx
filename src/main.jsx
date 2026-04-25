7f2b76dad3773abd59b87acc471a3502e4da9fc0
Varshitha596 <koilakondavarshapraveenkumar@gmail.com>
2026-04-25 11:48:50 +0530

    stake-hood 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './appy.jsx'
 // import Page from './component/UserProfile/index.jsx'
 
 createRoot(document.getElementById('root')).render(
