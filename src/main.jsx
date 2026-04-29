435fed880299f036cfcb60e600741a97a8a2b86f
Varshitha596 <koilakondavarshapraveenkumar@gmail.com>
2026-04-29 23:35:42 +0530

    in

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 //import App from './appy.jsx'
 // import Page from './component/UserProfile/index.jsx'
-import App from './App.jsx'
+//import App from './App.jsx'
+import App from './new.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
