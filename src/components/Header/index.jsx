7aa931d5bd9f00f93d6f1de06e6deac1c363f537
Varshitha596 <koilakondavarshapraveenkumar@gmail.com>
2026-05-01 23:29:14 +0530

    all students learder board

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,34 +1,15 @@
 import './index.css'
 
 const Header = () => (
-  <nav className="nav-header">
-    <div className="nav-content">
-      <p className="website-logo">RA</p>
-      <ul className="nav-menu">
-        <li>
-          <img
-            className="social-network-img"
-            src="https://assets.ccbp.in/frontend/react-js/projects-linkedin-img.png"
-            alt="Linked In"
-          />
-        </li>
-        <li>
-          <img
-            className="social-network-img"
-            src="https://assets.ccbp.in/frontend/react-js/projects-github-img.png"
-            alt="Git Hub"
-          />
-        </li>
-        <li>
-          <img
-            className="social-network-img"
-            src="https://assets.ccbp.in/frontend/react-js/projects-twitter-img.png"
-            alt="Twitter"
-          />
-        </li>
-      </ul>
+  <div className="header-container">
+    <div className="header-elements-container">
+      <img
+        className="logo"
+        src="https://assets.ccbp.in/frontend/hooks/leaderboard/nxt-code-logo-img.png"
+        alt="nxt code logo"
+      />
     </div>
-  </nav>
+  </div>
 )
 
 export default Header
---
+++ src/components/Leaderboard/index.jsx
@@ -0,0 +1,48 @@
+import './index.css'
+import { useEffect,useState } from 'react'
+import LeaderboardRow from '../LeaderboardRow/index.jsx'
+const Leaderboard = () => {
+const [LeaderboardData,setLeaderboardData]=useState([])
+
+  useEffect(() =>{
+    const getLeaderboardData= async() =>{
+const url='https://apis2.ccbp.in/leaderboard-v2'
+const response = await fetch(url)
+const responseData= await response.json()
+const formattedData=responseData.leaderboard_data.map((eachUser) =>(
+  {
+    id:eachUser.id,
+    rank:eachUser.rank,
+    name:eachUser.name,
+    profileImgUrl:eachUser.profile_image_url,
+    score:eachUser.score,
+    language:eachUser.language,
+    timeSpent:eachUser.time_spent,
+  }))
+setLeaderboardData(formattedData)
+    }
+    getLeaderboardData()
+  },[])
+  const renderLeaderboardHeader = () => (
+    <li className="leaderboard-header">
+      <p className="table-heading rank">Rank</p>
+      <p className="table-heading name">Name</p>
+      <p className="table-heading score">Score</p>
+      <p className="table-heading language">Language</p>
+      <p className="table-heading time-spent">Time Spent</p>
+    </li>
+  )
+
+  const renderLeaderboard = () => (
+    <ul className="leaderboard-table-container">
+      {renderLeaderboardHeader()}
+    {LeaderboardData.map(eachUser => 
+    (<LeaderboardRow key = {eachUser.id} userDetails={eachUser}/>
+    ))}
+    </ul>
+  )
+
+  return <div className="leaderboard-container">{renderLeaderboard()}</div>
+}
+
+export default Leaderboard
